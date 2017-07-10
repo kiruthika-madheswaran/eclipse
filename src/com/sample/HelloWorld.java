ce90176b1b4d95ce111349028dc34c52daa240c2
kiruthika <kiruthika.madheswaran@accoliteindia.com>
2017-07-10 19:03:12 +0530

    commiting from eclipse

---
+++ src/com/sample/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+public class HelloWorld {
+
+	public static void main(String args[]) {
+		
+		System.out.println("hello world");
+		for (int i = 0; i < 5; i++) {
+
+			System.out.println(i);
+
+		}
+
+	}
+
+}
